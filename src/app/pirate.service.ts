dafb68fcf185855f366321b4a9f4f8d8303ffec1
Nitin Srivastava <nitin.dev3d@gmail.com>
2023-02-28 06:13:40 +0000

    updating pirate services

--- src/app/pirate.service.ts
+++ src/app/pirate.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { PIRATES } from './mock-pirates';
+import { Pirate } from './pirate';
 @Injectable({
   providedIn: 'root'
 })
 export class PirateService {
 
   constructor() { }
+
+  getPirates(): Pirate[] {
+    return PIRATES
+  }
 }
--- src/app/straw-hat/straw-hat.component.ts
+++ src/app/straw-hat/straw-hat.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pirate } from '../pirate';
 import { PIRATES } from '../mock-pirates';
+import { PirateService } from '../pirate.service';
+
 @Component({
   selector: 'app-straw-hat',
   templateUrl: './straw-hat.component.html',
   styleUrls: ['./straw-hat.component.css']
 })
-export class StrawHatComponent {
+export class StrawHatComponent implements OnInit{
   captain: string = 'Monkey D. Luffy'
-  pirates = PIRATES
+  pirates: Pirate[] = []
   
   selectedPirate?: Pirate
+
+  constructor(private pirateService: PirateService) {}
+
+  getPirates() : void {
+    this.pirates = this.pirateService.getPirates()
+  }
 
   onSelect(pirate:Pirate): void
   {
     this.selectedPirate = pirate
   }
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getPirates()
+  }
 }
